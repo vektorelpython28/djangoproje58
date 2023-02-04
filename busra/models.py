306f9f1b63afb7d40eb87d5bb1c0c8d60e6ba725
vektorelpython28 <vektorelpython28@gmail.com>
2023-02-04 08:18:46 +0000

    04022023

--- busra/models.py
+++ busra/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BlogModel(models.Model):
+    baslik = models.CharField(max_length=200)
+    yazi = models.TextField()
+    kayitzaman = models.DateTimeField(default=timezone.now)
+    yayimzaman = models.DateTimeField(null=True,blank=True)
+
+    def yayimla(self):
+        self.yayimzaman = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.baslik
+
+    
