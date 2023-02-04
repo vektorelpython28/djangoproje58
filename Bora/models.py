df3c3af953e13bf7cea5180da4325b7f695c8d57
vektorelpython28 <vektorelpython28@gmail.com>
2023-02-04 08:04:58 +0000

    04022023

--- Bora/models.py
+++ Bora/models.py
@@ -3,12 +3,14 @@ from django.utils import timezone
 
 class MakaleModel(models.Model):
     makale_baslik = models.CharField(max_length=250)
+    makale_konu = models.CharField(max_length=100)
     makale_yazi = models.TextField()
-    makale_ozet = models.TextField()
     makale_kayitzaman = models.DateTimeField(default=timezone.now)
-    makale_yayimzaman = models.DateTimeField()
+    makale_yayimzaman = models.DateTimeField(null = True, blank = True)
     
     def yayimzamani(self):
         self.makale_yayimzaman = timezone.now()
+        self.save()
+
     def __str__(self):
         return self.baslik
--- Emre/models.py
+++ Emre/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-class ZINKModel(models.Model):
-    isim = models.CharField(max_length=200,verbose_name = "İsim")
+class EsporModel(models.Model):
+    isim = models.CharField(max_length=30,verbose_name = "İsim")
     nickname = models.CharField(max_length=20,verbose_name = "Kullanıcı Adı")
-    mainhero = models.CharField(max_length=10,verbose_name = "İsim")
+    mainhero = models.CharField(max_length=10,verbose_name = "Oynanan Ana Ajan", null = True)
+    takim = models.CharField(max_length=30,verbose_name = "Oynanan Takım")
     giristarih = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
