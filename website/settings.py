fda6e51a04750f0a9fbe8309aff5b961a90ff45e
vektorelpython28 <vektorelpython28@gmail.com>
2023-02-05 10:00:07 +0000

    05022023

--- website/settings.py
+++ website/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'sancakli',
     'nur',
     'Bora',
-    'mürüvet',
+    # 'mürüvet',
     'ediz',
     'busra',
     'taha',
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS  = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
